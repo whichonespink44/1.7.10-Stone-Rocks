16ffe1a6d3d219bfed6653e65305d273c2ba4706
WhichOnesPink <whichonespink@hokecraft.co.uk>
2016-05-08 12:18:58 +0100

    Added ItemRock.

---
+++ src/main/java/teamrtg/stonerocks/item/ItemRock.java
@@ -0,0 +1,16 @@
+package teamrtg.stonerocks.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemRock extends Item
+{
+
+	public static ItemRock itemRockStone;
+	public static ItemRock itemRockCobblestone;
+	
+    public ItemRock()
+    {
+		setCreativeTab(CreativeTabs.tabMisc);
+    }
+}
